53f38e07095ef0ce14a483a859d5d72c72d29ecf
JasLeu <jml858@hotmail.com>
2022-10-10 14:31:45 -0700

    added more changes

--- Main.java
+++ Main.java
@@ -1,31 +1,39 @@
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.Date;
 import java.util.ArrayList;
 
 public class Main {
+    public static String log = "";
     public static Scanner sc;
     
     public static void main(String[] args) throws InterruptedException {
         clearScreen();
-        System.out.println("You wake up to a loud scream coming from outside your bunker.");
-        TimeUnit.SECONDS.sleep(1);
-
         String scream = "AAAAAAAARRRRGGGGGHHHHHHHHHHH!!!!!!";
         for (int i = 0; i < scream.length(); i++) {
             System.out.print(scream.substring(i, i+1));
             // TimeUnit.MILLISECONDS.sleep(150 - (5*i));
         }
+        log += "AAAAAAAARRRRGGGGGHHHHHHHHHHH!!!!!!";
         TimeUnit.SECONDS.sleep(1);
 
-        int ans = promptUser("Do you want to (1) investigate or (2) go back to sleep?");
-        // if (false) {
-
-        // }
+        int ans = promptUser("...What is going on?\nShould I (1) investigate or (2) go back to sleep?", 5, "the entity esaped.");
+        if (ans == 1) {
+            ans = promptUser("You walk to your storage room and look around. You probab\nBring a weapon or a flashlight?", 5, "the entity escaped.");
+    
+        } else if (ans == 2) {
+            
+        }
     }
 
-    public static int promptUser(String question) {
+    public static int promptUser(String question, int seconds, String timedMessage) {
+        Date initialTime = new Date();
+        Date targetTime = new Date();
         ArrayList<Integer> allowedAnswers = new ArrayList<Integer>();
+        ArrayList<String> textAnswers = new ArrayList<String>();
         int ans;
+
+        log += "\n\n" + question;
 
         String tempQuestion = question;
         while (tempQuestion.indexOf("(") != -1) {
@@ -34,29 +42,76 @@ public class Main {
             tempQuestion = tempQuestion.replace(allowedAnswer, "");
         }
 
+        targetTime.setTime(initialTime.getTime() + (seconds * 1000));
+
         while (true) {
             clearScreen();
-            System.out.println(question);
+            System.out.println("You can input (0) to check the log!");
+
+            if (seconds != 0) {
+                System.out.println("You have " + seconds + " seconds to answer!");
+            }
+
+            System.out.println("\n" + question);
 
             try {
                 sc = new Scanner(System.in);
                 ans = sc.nextInt();
 
+                if (new Date().compareTo(targetTime) == 1 && seconds != 0) {
+                    clearScreen();
+                    System.out.println("You took too long... " + timedMessage);
+                    System.exit(1);
+                }
+
                 if (allowedAnswers.indexOf(ans) == -1) {
-                    throw new Exception("w");
+                    if (ans == 0) {
+                        targetTime = checkLog(Math.round((targetTime.getTime() - initialTime.getTime()) / 1000));
+                        throw new Exception("LOG");
+                    }
+
+                    throw new Exception("");
                 }
 
                 break;
             } catch (Exception e) {
-                System.out.println("Invalid answer! Try again!");
+                if (e.getMessage() != "LOG") {
+                    System.out.println("Invalid answer! Try again!");
+                }
                 try {
                     TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e1) {}
             }
         }
 
+        log += "\n" + ans;
         return ans;
     }
+
+    public static Date checkLog(int secondsLeft) {
+        clearScreen();
+        System.out.println(secondsLeft);
+        System.out.println(log);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {}
+
+        System.out.println("\n\nPress enter to continue...");
+        sc = new Scanner(System.in);
+        sc.nextLine();
+
+        Date newDate = new Date();
+        newDate.setTime(newDate.getTime() + (secondsLeft * 1000));
+
+        return newDate;
+    }
+
+    public void time(int secs) {
+        try {
+            TimeUnit.SECONDS.sleep(secs);
+        } catch (InterruptedException e) {}
+    }
+
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
